e5e78b35cf3643571a776e85f7a88f1ad531c47e
Trevor Guo <trevorguo@gmail.com>
2021-05-04 22:33:20 -0700

    Added pages and navbar

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+//Components
+import Navbar from './components/navbar'
+
+//Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home}/>
+            <Route exact path ="/login" component={login}/>
+            <Route exact path ="/signup" component={signup}/>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
